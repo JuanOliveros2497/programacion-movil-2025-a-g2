a8ae8532c0c9943a6e156b21897b29fb1b8071ce
Juan Esteban <oliverosduranj@gmail.com>
2025-05-27 12:30:57 -0500

    Eliminacion

--- FrontReact/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.corhuila',
-  appName: 'ToDo',
-  webDir: 'dist'
-};
-
-export default config;
